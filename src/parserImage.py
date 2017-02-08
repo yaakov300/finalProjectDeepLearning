88f10133fcd31977b637ba61b69a4212dfef71e4
yakov mushnik <yaacov300@gmail.com>
2017-02-08 17:35:09 +0200

    convert image to matrix as gray lavel

--- src/parserImage.py
+++ src/parserImage.py
@@ -1,22 +1,30 @@
-
+import PIL
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import Image, numpy
 from numpy.linalg import norm
+from resizeimage import resizeimage
+#save matrix as image
+import scipy.misc
 
-
-size = 10, 10
-img = Image.open('testimage.jpg',mode= 'r')
-img.thumbnail(size, Image.ANTIALIAS)
+#image size = size * size
+size = 5
+#open image
+img = Image.open('testimage.jpg')
+#resize image
+img = img.resize((size, size), Image.ANTIALIAS)
+#convert to Gray level
 matrix = numpy.asarray(img.convert('L'))
-#print matrix
-
+#normalize matrix
 linfnorm = norm(matrix, axis=1, ord=np.inf)
-matrix.astype(np.float) / linfnorm[:,None]
-print matrix.astype(np.float) / linfnorm[:,None]
+matrix = matrix.astype(np.float) / linfnorm[:,None]
 
-print np.reshape(matrix.astype(np.float) / linfnorm[:,None], (matrix.size, 1))
+matrix1 = np.reshape(matrix, (size*size, 1))
 
-plt.imshow(matrix, cmap = plt.get_cmap('gray'))
+vectorRezulte = x = np.array([1,0,0,0])
+
+print matrix1
+
+plt.imshow(matrix, cmap=plt.get_cmap('gray'))
 plt.show()

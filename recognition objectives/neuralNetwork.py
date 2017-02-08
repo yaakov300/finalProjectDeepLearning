06cec1fcaa018e5b682c77492d6a12b4cb19efe2
yakov mushnik <yaacov300@gmail.com>
2017-02-08 14:40:06 +0200

    add dominations of Network

--- recognition objectives/neuralNetwork.py
+++ recognition objectives/neuralNetwork.py
@@ -1,5 +1,14 @@
 import numpy as np
 
 class neuralNetwork(object):
-    def __init__(self, sizes):
-        pass
+
+    def __init__(self, sizesNetwork):
+        self.numberOfLayer = len(sizesNetwork)
+        self.sizes = sizesNetwork
+        self.biases = [np.random.randn(y, 1) for y in sizesNetwork[1:]]
+        self.weights = [np.random.randn(y, x)
+                        for x, y in zip(sizesNetwork[:-1], sizesNetwork[1:])]
+
+
+
+

7a69c17053131f24e0b1a6da6b4aad6a04b9f952
shimondavino <shimondv2@gmail.com>
2017-02-09 18:07:55 +0200

    resize the image convert to grayscale color convert image to array pixle normalized the array create pkl file for saving the image matrix

--- src/parserImage.py
+++ src/parserImage.py
@@ -1,49 +1,75 @@
-import numpy as np
-import numpy
 import PIL.Image as Image
 import glob
 import os
-from numpy.linalg import norm
+import numpy as np
+import cPickle
 
 #image size = size * size
-size = 5
+size = 28
 
 #source images dir
 src_dir = "img"
-image_list = []
-type_list = []
 
-for jpgfile in glob.iglob(os.path.join(src_dir, "*.jpg")):
-    # open image
-    img = Image.open(jpgfile)
-    # resize image
-    img = img.resize((size, size), Image.ANTIALIAS)
-    # convert to Gray level
-    img = img.convert('L')
-    pix_val = list(img.getdata())
-    print pix_val[:, None]
-    print ("===========\n")
-    matrix = numpy.asarray(img)
-    # print matrix
+pkl_file_name = "source.pkl";
 
 
-    # normalize matrix
-    linfnorm = norm(matrix, axis=1, ord=np.inf)
-    print linfnorm[:, None]
+
+def write_pkl_file(list, file_name):
+    # open the file for writing
+    fileObject = open(file_name, 'wb')
+
+    # this writes the object a to the
+    # file named 'testfile'
+    cPickle.dump(list, fileObject)
+
+    # here we close the fileObject
+    fileObject.close()
+
+def load_pkl_data(file_name):
+    try:
+        fileObject = open(file_name, 'rb')
+        training_data, validation_data, test_data = cPickle.load(fileObject)
+        return (training_data, validation_data, test_data)
+    except IOError:
+        userAnswer = raw_input("pklList.pkl not exist press yes to start training other to exit:")
+        if (userAnswer != 'yes'):
+            exit()
+    return None
 
 
-    matrix = matrix.astype(np.float) / linfnorm[:, None]
+def createPklFile():
+    image_list = []
+    type_list = []
 
-    image_list.append(matrix)
-    type_list.append(0)
-# print image_list
-training_data = [image_list,numpy.asarray(type_list)]
-# print training_data
-# matrix1 = np.reshape(matrix, (size*size, 1))
+    for jpgfile in glob.iglob(os.path.join(src_dir, "*.jpg")):
+        # open image and convert to Gray level
+        img = Image.open(jpgfile).convert('L')
+        # resize image
+        img = img.resize((size, size), Image.ANTIALIAS)
 
-# vectorRezulte = x = np.array([1,0,0,0])
+        # normalize image
+        pix_val = np.array(list(img.getdata())).astype('float')
+        pix_val = pix_val/np.linalg.norm(pix_val)
 
-# print matrix1
+        image_list.append(pix_val)
+        type_list.append(1)
 
-# imshow(matrix, cmap=get_cmap('gray'))
-# show()
+    image_list = np.array(image_list,dtype='float32')
+    type_list = np.array(type_list)
+
+
+    training_data = (image_list,type_list)
+    alidation_data = (image_list,np.array([2,2]))
+    test_data = (image_list,np.array([3,3]))
+    print training_data
+
+    write_bucket_pkl((training_data,alidation_data,test_data),pkl_file_name)
+
+
+
+print('-----------\n')
+
+
+tr_d, va_d, te_d = load_pkl_data(pkl_file_name)
+
+

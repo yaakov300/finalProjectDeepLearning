b839dde5605a24fd2ecd67d660cab12b3e6adc14
shimondavino <shimondv2@gmail.com>
2017-03-24 11:09:04 +0300

    parser the data file

--- src/parser/parserTest.py
+++ src/parser/parserTest.py
@@ -1,16 +1,43 @@
 import re
+import numpy as np
 
-file = open("vca_shape_learning.txt") #read original alignment file
+file = open("../../data/vca_shape_learning.txt") #read original alignment file
 fileContent = file.read()
 
 imageReg = re.compile("(?<!:)ImageFileName")
 imageList = imageReg.split(fileContent)[1:]
 
+imageArray =[]
+typeArray = []
+
 for image in imageList:
+
     templetReg = re.compile("(?<!:)Normal Template results")
     templetList = templetReg.split(image)
+    templetArray = []
     for templet in templetList[1:]:
-        pass
+        tmp = re.search(',0, \[(.*)]', templet)
+        tmp = tmp.group(1)
 
-print len(imageList)
-print imageList[0]
+        for num in tmp.split(';'):
+            templetArray.append(num)
+
+    typeArray.append(re.search('LearningClassId=(.*)',templetList[0]).group(1)[0]);
+    imageArray.append(np.array(templetArray))
+
+training_data_size = len(imageArray)* 5 / 7
+print training_data_size
+# Randomly shuffle data
+np.random.seed(training_data_size)
+shuffle_indices = np.random.permutation(np.arange(len(imageArray)))
+imageArray = np.array(imageArray)
+typeArray = np.array(typeArray)
+
+
+training_image_data = imageArray[shuffle_indices]
+training_type_data = typeArray[shuffle_indices]
+
+
+
+
+

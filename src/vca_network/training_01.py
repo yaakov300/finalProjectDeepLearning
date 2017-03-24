f038b3d7a70c823c7b7c69bd2285e63417922ec3
yakov mushnik <yaacov300@gmail.com>
2017-03-24 14:43:21 +0300

    init vca-network - Network, loader, training

---
+++ src/vca_network/training_01.py
@@ -0,0 +1,9 @@
+import mnist_loader
+import network
+
+#add data
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+#init neural networks
+net = network.Network([784, 30, 10])
+#start tarnining
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)

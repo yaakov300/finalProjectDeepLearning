798e37ac84962a117af6aeaead2909096d4867d0
yakov mushnik <yaacov300@gmail.com>
2017-05-30 23:01:41 +0300

    add CNN function

---
+++ src/classified/CNN_layer.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+
+def new_conv_layer(input, num_input_channels, filter_size, num_filters, use_pooling=True):
+
+    shape = [filter_size, filter_size,  num_input_channels, num_filters]
+
+    weights = new_weights(shape=shape)
+
+    biases = new_biases(length=num_filters)
+
+    layer = tf.nn.conv2d(input=input,
+                         filter=weights,
+                         strides=[1, 1, 1, 1],
+                         padding='SAME')
+
+    layer += biases
+
+    if use_pooling:
+        layer = tf.nn.max_pool(value=layer,
+                               ksize=[1, 2, 2, 1],
+                               strides=[1, 2, 2, 1],
+                               padding='SAME')
+    layer = tf.nn.relu(layer)
+    return layer, weights

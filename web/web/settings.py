20bd9949b6495ea3af2e97168cca78e5f38ba78c
shimondavino <shimondv2@gmail.com>
2017-06-20 22:38:09 +0300

    fix js file load

--- web/web/settings.py
+++ web/web/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -27,17 +26,28 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    # 'django.contrib.admin',
+    # 'django.contrib.auth',
+    # 'django.contrib.contenttypes',
+    # 'django.contrib.sessions',
+    # 'django.contrib.messages',
+    # 'django.contrib.staticfiles',
+
+    # 'rest_framework',
+    # 'snippets.apps.SnippetsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'website'
+    'django.contrib.staticfiles', # Ensure a comma ends this line
+    'django.contrib.humanize',
+    'rest_framework', # Add this line
+    'api',
+    'website',
 ]
 
 MIDDLEWARE = [
@@ -70,7 +80,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'web.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -100,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -114,8 +122,16 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(STATICFILES_DIRS, '/static/')
+SCRIPT_ROOT = (os.path.join(STATIC_ROOT,'/scripts/'),)
+SCRIPT_URL = '/scripts/'
+
--- web/website/urls.py
+++ web/website/urls.py
@@ -1,9 +1,16 @@
 # website/urls.py
 from django.conf.urls import url
 from website import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view()),
-    url(r'^apply/$', views.ApplayPageView.as_view()),
-    url(r'^train/$', views.TrainingPageView.as_view())
-]
+    url(r'^$', views.HomePageView.as_view(), name="deshboard"),
+    url(r'^apply/$', views.ApplayPageView.as_view(), name="apply"),
+    url(r'^train/$', views.TrainingPageView.as_view(),name="train"),
+    # url(r'static/')
+
+
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += script(settings.SCRIPT_URL, document_root=settings.SCRIPT_ROOT)
